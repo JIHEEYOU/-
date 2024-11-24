ca2ef398f9e12f9069117c8d6047bc2ee7ef01e7
JIHEEYOU <yujihui5@gmail.com>
2024-11-25 05:45:31 +0900

    6-2.restapi 기본코드 정리

--- 6.crm/2.restapi/app.js
+++ 6.crm/2.restapi/app.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const sqlite3 = require("sqlite3").verbose();
 const path = require("path");
+const morgan = require("morgan");
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
 const db = new sqlite3.Database("user-sample.db");
 
-//미들웨어
-app.use(express.static("public"));
+const logStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a",
+});
 
-//라우트
+// 미들웨어
+app.use(express.static("public"));
+app.use(morgan("combined", { stream: logStream }));
+app.use(morgan("dev"));
+
+// combined - 아파치 서버 로그 포멧
+// common - 요약된 형태
+// dev - 개발시 유용한 모드
+// tiny
+// short
+
+// app.use(myLogger)
+
+function myLogger(req, res, next) {
+  console.log(`LOG: ${req.method} ${req.url}`);
+  next();
+}
+
+// 라우트
+// 시스템 호출용 API 라우트
 app.get("/api/users", (req, res) => {
   const query = "SELECT * FROM users";
   db.all(query, [], (err, rows) => {
@@ -18,18 +40,26 @@ app.get("/api/users", (req, res) => {
 });
 
 app.get("/api/users/:id", (req, res) => {
-  const query = "SELECT * FROM users WHERE Id=?";
+  const userId = req.params.id;
+
+  const query = "SELECT * FROM users WHERE Id = ?";
   db.get(query, [userId], (err, rows) => {
     res.json(rows);
   });
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve("public/users.html"));
-  res.sendFile(path.join(__dirname, "public", "user.html"));
+// 사용자 페이지용 라우트
+app.get("/users/:id", (req, res) => {
+  res.sendFile(path.resolve("public/user_detail.html"));
 });
 
-//서버시작
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve("public/users.html"));
+  // res.sendFile(path.join(__dirname, 'public', 'users.html'));
+});
+
+// 서버 시작
 app.listen(port, () => {
-  console.log("서버 레디");
+  console.log("CRM Server is ready to start...");
+  console.log(`Server is ready on http://localhost:${port}`);
 });
--- 6.crm/2.restapi/public/js/users.js
+++ 6.crm/2.restapi/public/js/users.js
@@ -1,7 +1,5 @@
-const { response } = require("express");
-
 const searchButton = document.getElementById("search-button");
-const searchInput = document.getElementById("seacrch-name");
+const searchInput = document.getElementById("search-name");
 
 searchButton.addEventListener("click", () => {
   searchName = searchInput.value;
@@ -10,47 +8,43 @@ searchButton.addEventListener("click", () => {
 
 function fetchUsers(searchName) {
   fetch(`/api/users`)
-    .then((reponse) => reponse.json())
+    .then((response) => response.json())
     .then((data) => {
-      //렌더링 코드 작성
+      // 랜더링 코드 작성
       renderTable(data);
-      console.log(data);
     });
 
-  //esponse= await fetch(`/api/users`);
-  //const data = await response.json();
+  // const response = await fetch(`/api/users`);
+  // const data = await response.json();
 }
 
 function renderTable(data) {
   const tableHeader = document.getElementById("table-header");
   const tableBody = document.getElementById("table-body");
 
-  //지우고 출발하자..
+  // 지우고 출발하자...
   tableHeader.innerHTML = "";
   tableBody.innerHTML = "";
 
-  //헤더 그리기 tr안에 th그리기
+  // 헤더 그리기: tr 안에 th 그리기
   const headerRow = document.createElement("tr");
-  const fileds = Object.keys(data[0]);
-
-  fileds.forEach((f) => {
+  const fields = Object.keys(data[0]);
+  fields.forEach((f) => {
     if (f !== "Id" && f !== "Address") {
       const th = document.createElement("th");
       th.textContent = f;
       headerRow.appendChild(th);
     }
   });
-
   tableHeader.appendChild(headerRow);
-  console.log(fields);
 
-  //바디 그리기 : tr 안에 td그리기
-  //Object.entries() 이걸 통해서 td 추가한다.
+  // 바디 그리기: tr안에 td 그리기
+  // Object.entries() 이걸 통해서 td 추가한다...
 
   data.forEach((row) => {
     const bodyRow = document.createElement("tr");
     bodyRow.addEventListener("click", () => {
-      window.location = `/user/${row.Id}`;
+      window.location = `/users/${row.Id}`;
     });
 
     for (const [key, value] of Object.entries(row)) {
@@ -64,4 +58,4 @@ function renderTable(data) {
   });
 }
 
-fetchUsers(""); //시작할 때는 그냥 빈값으로 검색, 즉 모든 사용자 다 출력
+fetchUsers(""); // 시작할때는 그냥 빈값으로 검색, 즉 모든 사용자 다 출력
